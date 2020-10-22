6eef62c71a65770458c03021cfecdda20987a9bc
Alfonso James <ajaeguy@gmail.com>
2020-10-22 15:26:18 -0400

    added ajax get method and API key to js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
-// https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=cd9a0bbdc14e65849d8ca690b227df87
+// $("button").on("click", function() {
+    
+    // var city = $(this).attr("data-person");
+    var city ="Orlando";
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=cd9a0bbdc14e65849d8ca690b227df87";
+    
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+      });
+    
+// })
